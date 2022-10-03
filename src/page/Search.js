69b27079adc606ba9cb8fe92255f2a92f0527329
msss32 <msss14@naver.com>
2022-10-03 21:17:17 +0900

    toy

---
+++ src/page/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Header } from "../component";
+
+const Search = () => {
+  return (
+    <div>
+      <Header
+        path_main="/"
+        path_join="/join"
+        path_board="/board"
+        path_search="/search"
+        path_user="/mypage"
+      />
+    </div>
+  );
+};
+
+export default Search;
